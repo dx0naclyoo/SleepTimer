2a540979cdec3fc8d4b74ef88aed3f0fb0c8599d
dx0naclyoo <konyzhenkovdanil@gmail.com>
2024-05-14 15:25:02 +0300

    Dialog Window add seconds for shutdown

--- sleepmanager/UI/window.py
+++ sleepmanager/UI/window.py
@@ -2,8 +2,9 @@ import sys
 from typing import List
 
 from PyQt6 import QtGui, QtCore
-from PyQt6.QtCore import QDateTime
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QPushButton
+from PyQt6.QtCore import QDateTime, pyqtSignal
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QPushButton, QDialog, QVBoxLayout, \
+    QLineEdit
 
 UI_BASE_DIR = sys.path[0]
 
@@ -18,11 +19,69 @@ class GUIApp(QApplication):
             in_widget.show()
 
 
+class AddDateDialog(QDialog):
+    dataEntered = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Введите секунды")
+
+        layout = QVBoxLayout()
+
+        self.seconds_label = QLabel("Секунды:")
+        self.seconds_input = QLineEdit()
+
+        self.result_label = QLabel()
+
+        self.dialog_add_button = QPushButton("Добавить")
+        self.dialog_add_button.setStyleSheet("QPushButton { background-color: #4CAF50; color: white; "
+                                             "border: 1px solid #357035; "
+                                             "border-radius: 5px; min-width: 50px; min-height: 30px; }"
+                                             "QPushButton:hover { background-color: #45a049; }"
+                                             "QPushButton:pressed { background-color: #357035; }")
+
+        self.seconds_input.textChanged.connect(self.calculate_time)
+        self.dialog_add_button.clicked.connect(lambda: self.send_data(self.seconds_input.text()))
+
+        layout.addWidget(self.seconds_label)
+        layout.addWidget(self.seconds_input)
+        layout.addWidget(self.result_label)
+        layout.addWidget(self.dialog_add_button)
+
+        self.setLayout(layout)
+
+    def send_data(self, sec_inp):
+        if sec_inp.isdigit():
+            seconds = int(sec_inp)
+            self.dataEntered.emit(seconds)
+            self.accept()
+
+        self.seconds_input.clear()
+
+    def calculate_time(self):
+        try:
+            seconds = int(self.seconds_input.text())
+            if seconds < 0:
+                raise ValueError("Отрицательное значение")
+
+            hours = seconds // 3600
+            minutes = (seconds % 3600) // 60
+            remaining_seconds = seconds % 60
+
+            self.result_label.setText(f"{hours} часов {minutes} минут {remaining_seconds} секунд")
+        except ValueError as e:
+            self.result_label.setText("Ошибка: " + str(e))
+
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.timer_label = QLabel()
         self.working_clock = QtCore.QTimer()
+        self.delete_button = QPushButton()
+        self.add_button = QPushButton()
+        self.dialog = AddDateDialog(self)
+        self.shutdowns: int = 0
 
         self.init_ui()
 
@@ -33,12 +92,14 @@ class MainWindow(QWidget):
 
         main_layout = QHBoxLayout(self)
 
+        self.dialog.setGeometry(500, 350, 200, 50)
+
         self.timer_label = QLabel(QtCore.QDateTime.currentDateTime().toString('HH:mm:ss\ndd.MM.yyyy'))
         self.timer_label.setStyleSheet("QLabel "
                                        "{ "
                                        "color: white; "
                                        "font-size: 24px; "
-                                       "padding: 10px 40px 10px 40px; "
+                                       "padding: 10px 25px 10px 25px; "
                                        "background-color: #333; "
                                        "text-align: center; "
                                        "border-radius: 10px; }")
@@ -52,18 +113,20 @@ class MainWindow(QWidget):
         self.add_button.setFixedSize(100, 30)
         self.add_button.setStyleSheet("QPushButton { background-color: #4CAF50; color: white; "
                                       "border: 1px solid #357035; "
-                                      "border-radius: 5px; min-width: 120px; min-height: 30px; }"
+                                      "border-radius: 5px; min-width: 140px; min-height: 30px; }"
                                       "QPushButton:hover { background-color: #45a049; }"
                                       "QPushButton:pressed { background-color: #357035; }")
+        self.add_button.clicked.connect(lambda: self.button_click("add"))
 
         self.delete_button = QPushButton()
         self.delete_button.setText('Delete')
         self.delete_button.setFixedSize(100, 30)
         self.delete_button.setStyleSheet("QPushButton { background-color: #f44336; color: white; "
                                          "border: 1px solid #801515; "
-                                         "border-radius: 5px; min-width: 120px; min-height: 30px; }"
+                                         "border-radius: 5px; min-width: 140px; min-height: 30px; }"
                                          "QPushButton:hover { background-color: #d32f2f; }"
                                          "QPushButton:pressed { background-color: #801515; }")
+        self.delete_button.clicked.connect(lambda: self.button_click("delete"))
 
         buttons_layout = QHBoxLayout()  # Создание нового горизонтального макета для кнопок
         buttons_layout.addWidget(self.add_button)
@@ -78,6 +141,18 @@ class MainWindow(QWidget):
     def update_clock(self):
         self.timer_label.setText(QDateTime.currentDateTime().toString('HH:mm:ss\ndd.MM.yyyy'))
 
+    def button_click(self, button):
+        if button == "add":
+            print("Нажата кнопка: add")
+            self.dialog.show()
+            self.dialog.dataEntered.connect(self.handler_seconds_input)
+            self.dialog.exec()
+
+        else:
+            print("Нажата кнопка: delete")
+
+    def handler_seconds_input(self, seconds):
+        print("Seconds:", seconds)
 
 if __name__ == '__main__':
     app = GUIApp(sys.argv)
